ccb31a71f97d6c2a7b59366972ce4e469096bc05
philoteer <cdac1234@gmail.com>
2017-12-10 15:54:34 -0800

    Add files via upload

--- test.py
+++ test.py
@@ -5,32 +5,36 @@ import pandas as pd
 import numpy as np
 
 #n value
-n = 3
+#n = 3
 iterations = 100
 
 df = pd.read_csv("feature.csv", header = 0)
 #shuffle
 df = df.sample(frac=1).reset_index(drop=True)
 
-rate = 0
-for i in range (0, iterations):
-	#training set
-	#https://stackoverflow.com/questions/24147278/how-do-i-create-test-and-train-samples-from-one-dataframe-with-pandas
-	mask = np.random.rand(len(df)) < 0.8
-	train = df[mask]
-	#test set
-	test = df[~mask]
+#uncomment to remove the frequency feature.
+#df = df.drop(['frequency'],1)
 
-	y = train['class']
-	x = train.drop(['class'],1)
+for n in range (1, 8):
+	rate = 0
+	for i in range (0, iterations):
+		#training set
+		#https://stackoverflow.com/questions/24147278/how-do-i-create-test-and-train-samples-from-one-dataframe-with-pandas
+		mask = np.random.rand(len(df)) < 0.8
+		train = df[mask]
+		#test set
+		test = df[~mask]
 
-	knn = KNeighborsClassifier(algorithm='auto', leaf_size=30, metric="euclidean", metric_params=None, n_jobs=1, n_neighbors=n, p=2, weights='uniform')
+		y = train['class']
+		x = train.drop(['class'],1)
 
-	knn.fit(x,y)
+		knn = KNeighborsClassifier(algorithm='auto', leaf_size=30, metric="euclidean", metric_params=None, n_jobs=1, n_neighbors=n, p=2, weights='uniform')
 
-	test_no_class = test.drop(['class'],1)
+		knn.fit(x,y)
 
-	a = knn.predict(test_no_class)
-	rate += sum(a == test['class'])/float(len(test) * iterations)
-	
-print rate
+		test_no_class = test.drop(['class'],1)
+
+		a = knn.predict(test_no_class)
+		rate += sum(a == test['class'])/float(len(test) * iterations)
+	print n
+	print rate
